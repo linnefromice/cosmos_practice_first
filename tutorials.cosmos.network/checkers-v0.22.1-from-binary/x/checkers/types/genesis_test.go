cab99d27cd091b992416de793753318c274ece27
linnefromice <linnefromice@gmail.com>
2022-11-17 17:24:13 +0900

    follow 3-stored-game - add: unit test

--- tutorials.cosmos.network/checkers-v0.22.1-from-binary/x/checkers/types/genesis_test.go
+++ tutorials.cosmos.network/checkers-v0.22.1-from-binary/x/checkers/types/genesis_test.go
@@ -63,3 +63,13 @@ func TestGenesisState_Validate(t *testing.T) {
 		})
 	}
 }
+
+func TestDefaultGenesisState_ExpectedInitialNextID(t *testing.T) {
+	require.EqualValues(
+		t,
+		&types.GenesisState{
+			StoredGameList: []types.StoredGame{},
+			SystemInfo:     types.SystemInfo{uint64(1)},
+		},
+		types.DefaultGenesis())
+}
