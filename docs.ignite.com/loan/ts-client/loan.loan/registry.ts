e2617714e00f72389cdf01b31471d0ec0e4bb6c1
linnefromice <linnefromice@gmail.com>
2022-11-20 00:33:24 +0900

    exec 'ignite scaffold message request-loan amount fee collateral deadline'

--- docs.ignite.com/loan/ts-client/loan.loan/registry.ts
+++ docs.ignite.com/loan/ts-client/loan.loan/registry.ts
@@ -1,6 +1,8 @@
 import { GeneratedType } from "@cosmjs/proto-signing";
+import { MsgRequestLoan } from "./types/loan/loan/tx";
 
 const msgTypes: Array<[string, GeneratedType]>  = [
+    ["/loan.loan.MsgRequestLoan", MsgRequestLoan],
     
 ];
 
