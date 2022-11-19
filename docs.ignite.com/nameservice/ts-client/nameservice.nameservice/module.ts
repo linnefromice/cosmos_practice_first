2afe4b6661654f4b261b8f8655786893175c3466
linnefromice <linnefromice@gmail.com>
2022-11-19 14:52:25 +0900

    exec 'ignite scaffold message delete-name name'

--- docs.ignite.com/nameservice/ts-client/nameservice.nameservice/module.ts
+++ docs.ignite.com/nameservice/ts-client/nameservice.nameservice/module.ts
@@ -7,17 +7,12 @@ import { msgTypes } from './registry';
 import { IgniteClient } from "../client"
 import { MissingWalletError } from "../helpers"
 import { Api } from "./rest";
-import { MsgSetName } from "./types/nameservice/nameservice/tx";
 import { MsgBuyName } from "./types/nameservice/nameservice/tx";
+import { MsgSetName } from "./types/nameservice/nameservice/tx";
+import { MsgDeleteName } from "./types/nameservice/nameservice/tx";
 
 
-export { MsgSetName, MsgBuyName };
-
-type sendMsgSetNameParams = {
-  value: MsgSetName,
-  fee?: StdFee,
-  memo?: string
-};
+export { MsgBuyName, MsgSetName, MsgDeleteName };
 
 type sendMsgBuyNameParams = {
   value: MsgBuyName,
@@ -25,13 +20,29 @@ type sendMsgBuyNameParams = {
   memo?: string
 };
 
+type sendMsgSetNameParams = {
+  value: MsgSetName,
+  fee?: StdFee,
+  memo?: string
+};
+
+type sendMsgDeleteNameParams = {
+  value: MsgDeleteName,
+  fee?: StdFee,
+  memo?: string
+};
+
+
+type msgBuyNameParams = {
+  value: MsgBuyName,
+};
 
 type msgSetNameParams = {
   value: MsgSetName,
 };
 
-type msgBuyNameParams = {
-  value: MsgBuyName,
+type msgDeleteNameParams = {
+  value: MsgDeleteName,
 };
 
 
@@ -52,20 +63,6 @@ export const txClient = ({ signer, prefix, addr }: TxClientOptions = { addr: "ht
 
   return {
 		
-		async sendMsgSetName({ value, fee, memo }: sendMsgSetNameParams): Promise<DeliverTxResponse> {
-			if (!signer) {
-					throw new Error('TxClient:sendMsgSetName: Unable to sign Tx. Signer is not present.')
-			}
-			try {			
-				const { address } = (await signer.getAccounts())[0]; 
-				const signingClient = await SigningStargateClient.connectWithSigner(addr,signer,{registry, prefix});
-				let msg = this.msgSetName({ value: MsgSetName.fromPartial(value) })
-				return await signingClient.signAndBroadcast(address, [msg], fee ? fee : defaultFee, memo)
-			} catch (e: any) {
-				throw new Error('TxClient:sendMsgSetName: Could not broadcast Tx: '+ e.message)
-			}
-		},
-		
 		async sendMsgBuyName({ value, fee, memo }: sendMsgBuyNameParams): Promise<DeliverTxResponse> {
 			if (!signer) {
 					throw new Error('TxClient:sendMsgBuyName: Unable to sign Tx. Signer is not present.')
@@ -80,6 +77,42 @@ export const txClient = ({ signer, prefix, addr }: TxClientOptions = { addr: "ht
 			}
 		},
 		
+		async sendMsgSetName({ value, fee, memo }: sendMsgSetNameParams): Promise<DeliverTxResponse> {
+			if (!signer) {
+					throw new Error('TxClient:sendMsgSetName: Unable to sign Tx. Signer is not present.')
+			}
+			try {			
+				const { address } = (await signer.getAccounts())[0]; 
+				const signingClient = await SigningStargateClient.connectWithSigner(addr,signer,{registry, prefix});
+				let msg = this.msgSetName({ value: MsgSetName.fromPartial(value) })
+				return await signingClient.signAndBroadcast(address, [msg], fee ? fee : defaultFee, memo)
+			} catch (e: any) {
+				throw new Error('TxClient:sendMsgSetName: Could not broadcast Tx: '+ e.message)
+			}
+		},
+		
+		async sendMsgDeleteName({ value, fee, memo }: sendMsgDeleteNameParams): Promise<DeliverTxResponse> {
+			if (!signer) {
+					throw new Error('TxClient:sendMsgDeleteName: Unable to sign Tx. Signer is not present.')
+			}
+			try {			
+				const { address } = (await signer.getAccounts())[0]; 
+				const signingClient = await SigningStargateClient.connectWithSigner(addr,signer,{registry, prefix});
+				let msg = this.msgDeleteName({ value: MsgDeleteName.fromPartial(value) })
+				return await signingClient.signAndBroadcast(address, [msg], fee ? fee : defaultFee, memo)
+			} catch (e: any) {
+				throw new Error('TxClient:sendMsgDeleteName: Could not broadcast Tx: '+ e.message)
+			}
+		},
+		
+		
+		msgBuyName({ value }: msgBuyNameParams): EncodeObject {
+			try {
+				return { typeUrl: "/nameservice.nameservice.MsgBuyName", value: MsgBuyName.fromPartial( value ) }  
+			} catch (e: any) {
+				throw new Error('TxClient:MsgBuyName: Could not create message: ' + e.message)
+			}
+		},
 		
 		msgSetName({ value }: msgSetNameParams): EncodeObject {
 			try {
@@ -89,11 +122,11 @@ export const txClient = ({ signer, prefix, addr }: TxClientOptions = { addr: "ht
 			}
 		},
 		
-		msgBuyName({ value }: msgBuyNameParams): EncodeObject {
+		msgDeleteName({ value }: msgDeleteNameParams): EncodeObject {
 			try {
-				return { typeUrl: "/nameservice.nameservice.MsgBuyName", value: MsgBuyName.fromPartial( value ) }  
+				return { typeUrl: "/nameservice.nameservice.MsgDeleteName", value: MsgDeleteName.fromPartial( value ) }  
 			} catch (e: any) {
-				throw new Error('TxClient:MsgBuyName: Could not create message: ' + e.message)
+				throw new Error('TxClient:MsgDeleteName: Could not create message: ' + e.message)
 			}
 		},
 		
--- docs.ignite.com/nameservice/vue/src/store/generated/nameservice.nameservice/index.ts
+++ docs.ignite.com/nameservice/vue/src/store/generated/nameservice.nameservice/index.ts
@@ -128,19 +128,6 @@ export default {
 		},
 		
 		
-		async sendMsgSetName({ rootGetters }, { value, fee = [], memo = '' }) {
-			try {
-				const client=await initClient(rootGetters)
-				const result = await client.NameserviceNameservice.tx.sendMsgSetName({ value, fee: {amount: fee, gas: "200000"}, memo })
-				return result
-			} catch (e) {
-				if (e == MissingWalletError) {
-					throw new Error('TxClient:MsgSetName:Init Could not initialize signing client. Wallet is required.')
-				}else{
-					throw new Error('TxClient:MsgSetName:Send Could not broadcast Tx: '+ e.message)
-				}
-			}
-		},
 		async sendMsgBuyName({ rootGetters }, { value, fee = [], memo = '' }) {
 			try {
 				const client=await initClient(rootGetters)
@@ -154,7 +141,46 @@ export default {
 				}
 			}
 		},
+		async sendMsgSetName({ rootGetters }, { value, fee = [], memo = '' }) {
+			try {
+				const client=await initClient(rootGetters)
+				const result = await client.NameserviceNameservice.tx.sendMsgSetName({ value, fee: {amount: fee, gas: "200000"}, memo })
+				return result
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgSetName:Init Could not initialize signing client. Wallet is required.')
+				}else{
+					throw new Error('TxClient:MsgSetName:Send Could not broadcast Tx: '+ e.message)
+				}
+			}
+		},
+		async sendMsgDeleteName({ rootGetters }, { value, fee = [], memo = '' }) {
+			try {
+				const client=await initClient(rootGetters)
+				const result = await client.NameserviceNameservice.tx.sendMsgDeleteName({ value, fee: {amount: fee, gas: "200000"}, memo })
+				return result
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgDeleteName:Init Could not initialize signing client. Wallet is required.')
+				}else{
+					throw new Error('TxClient:MsgDeleteName:Send Could not broadcast Tx: '+ e.message)
+				}
+			}
+		},
 		
+		async MsgBuyName({ rootGetters }, { value }) {
+			try {
+				const client=initClient(rootGetters)
+				const msg = await client.NameserviceNameservice.tx.msgBuyName({value})
+				return msg
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgBuyName:Init Could not initialize signing client. Wallet is required.')
+				} else{
+					throw new Error('TxClient:MsgBuyName:Create Could not create message: ' + e.message)
+				}
+			}
+		},
 		async MsgSetName({ rootGetters }, { value }) {
 			try {
 				const client=initClient(rootGetters)
@@ -168,16 +194,16 @@ export default {
 				}
 			}
 		},
-		async MsgBuyName({ rootGetters }, { value }) {
+		async MsgDeleteName({ rootGetters }, { value }) {
 			try {
 				const client=initClient(rootGetters)
-				const msg = await client.NameserviceNameservice.tx.msgBuyName({value})
+				const msg = await client.NameserviceNameservice.tx.msgDeleteName({value})
 				return msg
 			} catch (e) {
 				if (e == MissingWalletError) {
-					throw new Error('TxClient:MsgBuyName:Init Could not initialize signing client. Wallet is required.')
+					throw new Error('TxClient:MsgDeleteName:Init Could not initialize signing client. Wallet is required.')
 				} else{
-					throw new Error('TxClient:MsgBuyName:Create Could not create message: ' + e.message)
+					throw new Error('TxClient:MsgDeleteName:Create Could not create message: ' + e.message)
 				}
 			}
 		},
