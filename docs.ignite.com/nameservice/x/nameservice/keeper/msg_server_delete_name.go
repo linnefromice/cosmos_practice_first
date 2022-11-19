a1c81e208e4ffbc83661fc856679e03b601d8b95
linnefromice <linnefromice@gmail.com>
2022-11-19 16:15:43 +0900

    update: implement 'delete name'

--- docs.ignite.com/nameservice/x/nameservice/keeper/msg_server_delete_name.go
+++ docs.ignite.com/nameservice/x/nameservice/keeper/msg_server_delete_name.go
@@ -3,15 +3,24 @@ package keeper
 import (
 	"context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"nameservice/x/nameservice/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) DeleteName(goCtx context.Context, msg *types.MsgDeleteName) (*types.MsgDeleteNameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	whois, isFound := k.GetWhois(ctx, msg.Name)
 
+	if !isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "Name doesn't exist")
+	}
+	if !(whois.Owner == msg.Creator) {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Incorrect Owner")
+	}
+
+	k.RemoveWhois(ctx, msg.Name)
 	return &types.MsgDeleteNameResponse{}, nil
 }
