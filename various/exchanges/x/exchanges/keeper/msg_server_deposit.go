5ad94d18c416b87fe0b09ea35154ebf1a0eb7e40
linnefromice <linnefromice@gmail.com>
2022-11-24 11:26:04 +0900

    update: deposit msg

--- various/exchanges/x/exchanges/keeper/msg_server_deposit.go
+++ various/exchanges/x/exchanges/keeper/msg_server_deposit.go
@@ -2,16 +2,32 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
 	"exchanges/x/exchanges/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) Deposit(goCtx context.Context, msg *types.MsgDeposit) (*types.MsgDepositResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	sender, _ := sdk.AccAddressFromBech32(msg.Creator)
 
-	// TODO: Handling the message
-	_ = ctx
+	err := k.bankKeeper.SendCoinsFromAccountToModule(ctx, sender, types.ModuleName, sdk.NewCoins(msg.Amount))
+	if err != nil {
+		return &types.MsgDepositResponse{}, err
+	}
+
+	lpDenom := fmt.Sprintf("share%s", msg.Amount.Denom)
+	lpCoin := sdk.NewCoin(lpDenom, msg.Amount.Amount)
+	err = k.bankKeeper.MintCoins(ctx, types.ModuleName, sdk.NewCoins(lpCoin))
+	if err != nil {
+		return &types.MsgDepositResponse{}, err
+	}
+	err = k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, sender, sdk.NewCoins(lpCoin))
+	if err != nil {
+		return &types.MsgDepositResponse{}, err
+	}
 
 	return &types.MsgDepositResponse{}, nil
 }
--- various/exchanges/x/exchanges/types/expected_keepers.go
+++ various/exchanges/x/exchanges/types/expected_keepers.go
@@ -7,12 +7,19 @@ import (
 
 // AccountKeeper defines the expected account keeper used for simulations (noalias)
 type AccountKeeper interface {
+	NewAccount(sdk.Context, types.AccountI) types.AccountI
 	GetAccount(ctx sdk.Context, addr sdk.AccAddress) types.AccountI
-	// Methods imported from account should be defined here
+	GetModuleAddress(moduleName string) sdk.AccAddress
+	GetModuleAddressAndPermissions(moduleName string) (sdk.AccAddress, []string)
+	GetModuleAccountAndPermissions(ctx sdk.Context, moduleName string) (types.ModuleAccountI, []string)
+	SetModuleAccount(ctx sdk.Context, macc types.ModuleAccountI)
+	HasAccount(ctx sdk.Context, addr sdk.AccAddress) bool
 }
 
 // BankKeeper defines the expected interface needed to retrieve account balances.
 type BankKeeper interface {
 	SpendableCoins(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
-	// Methods imported from bank should be defined here
+	MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
+	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 }
