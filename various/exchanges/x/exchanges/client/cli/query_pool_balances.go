a9cb88782a194ede57de6eeb2ceb4512245f6291
linnefromice <linnefromice@gmail.com>
2022-11-24 16:21:46 +0900

    exec 'ignite scaffold query pool-balances --response balances:array.coin'

---
+++ various/exchanges/x/exchanges/client/cli/query_pool_balances.go
@@ -0,0 +1,42 @@
+package cli
+
+import (
+	"strconv"
+
+	"exchanges/x/exchanges/types"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+)
+
+var _ = strconv.Itoa(0)
+
+func CmdPoolBalances() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "pool-balances",
+		Short: "Query pool-balances",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryPoolBalancesRequest{}
+
+			res, err := queryClient.PoolBalances(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
